3aa79e2770b6574b6c6b125ceefbae67ca570062
mathnm <40720588+mathnm@users.noreply.github.com>
2020-07-22 10:54:59 -0300

    connector

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
         primaryStage.setTitle("Stock Management");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
